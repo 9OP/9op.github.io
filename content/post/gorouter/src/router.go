a854d719ae6d38272a53c8dcf95db0535ac085f6
Martin <martin@cardiologs.com>
2021-05-31 22:54:50 +0200

    Revert stable

--- content/post/gorouter/src/router.go
+++ content/post/gorouter/src/router.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func split(path string) []string {
+	path = strings.TrimSpace(path)
+	path = strings.TrimPrefix(path, "/")
+	path = strings.TrimSuffix(path, "/")
+	return strings.Split(path, "/")
+}
+
+type Router struct {
+	trie *node
+	// ...
+}
+
+func NewRouter() *Router {
+	return &Router{
+		trie: newNode(),
+	}
+}
+
+func (router *Router) Handle(path, method string, h http.Handler) {
+	node := router.trie.append(split(path))
+	node.handlers[method] = h
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			http.Error(w, "server error", http.StatusInternalServerError)
+		}
+	}()
+
+	handler := http.NotFoundHandler()
+
+	if h := router.match(r); h != nil {
+		handler = h
+	}
+	handler.ServeHTTP(w, r)
+}
+
+func (router *Router) match(r *http.Request) http.Handler {
+	if node := router.trie.search(split(r.URL.Path)); node != nil {
+		return node.handlers[r.Method]
+	}
+	return nil
+}
--- content/post/gorouter/src/trie.go
+++ content/post/gorouter/src/trie.go
@@ -1,1 +1,50 @@
 package main
+
+import "net/http"
+
+type node struct {
+	handlers map[string]http.Handler
+	leaves   map[string]*node
+}
+
+func newNode() *node {
+	return &node{
+		handlers: map[string]http.Handler{},
+		leaves:   map[string]*node{},
+	}
+}
+
+func (node *node) getLeaf(v string) *node {
+	if node, ok := node.leaves[v]; ok {
+		return node
+	}
+	return nil
+}
+
+func (node *node) append(path []string) *node {
+	if len(path) == 0 {
+		return node
+	}
+	component := path[0]
+
+	leaf := node.getLeaf(component)
+	if leaf == nil {
+		node.leaves[component] = newNode()
+		leaf = node.leaves[component]
+	}
+
+	return leaf.append(path[1:])
+}
+
+func (node *node) search(path []string) *node {
+	if len(path) == 0 {
+		return node
+	}
+
+	leaf := node.getLeaf(path[0])
+	if leaf == nil {
+		return nil
+	}
+
+	return leaf.search(path[1:])
+}
